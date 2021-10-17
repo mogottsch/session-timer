391ae19098ab0d54235d5688e2cced18e1d08228
Moritz <gottschling.moritz@gmail.com>
2021-10-17 17:56:35 +0200

    Format timers

--- content-script.js
+++ content-script.js
@@ -20,19 +20,43 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   onStart(request, sendResponse);
 });
 
+// format time in milliseconds to hh:mm:ss
+const formatTime = (time, maxTime) => {
+  console.log(time, maxTime, maxTime > 1000 * 60);
+  const h = Math.floor(time / (1000 * 60 * 60));
+  const m = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const s = Math.floor((time % (1000 * 60)) / 1000);
+
+  // add leading zero
+  const hDisplay = h < 10 ? "0" + h : h;
+  const mDisplay = m < 10 ? "0" + m : m;
+  const sDisplay = s < 10 ? "0" + s : s;
+
+  // display depending on size of max time
+  if (maxTime > 1000 * 60 * 60) {
+    return `${hDisplay}:${mDisplay}:${sDisplay}`;
+  }
+  if (maxTime > 1000 * 60) {
+    return `${mDisplay}:${sDisplay}`;
+  }
+  return sDisplay;
+};
+
 const renderTimers = () => {
   const timerContainer = document.getElementById("timer-content");
   timerContainer.innerHTML = "";
 
   const table = document.createElement("table");
   const hosts = Object.keys(timers).sort();
+  const maxTime = Math.max(...Object.values(timers));
+
   for (const host of hosts) {
     const row = document.createElement("tr");
 
     const hostCell = document.createElement("td");
     hostCell.innerText = host;
     const timeCell = document.createElement("td");
-    timeCell.innerText = Math.round(timers[host] / 1000);
+    timeCell.innerText = formatTime(timers[host], maxTime);
 
     row.appendChild(hostCell);
     row.appendChild(timeCell);
@@ -51,8 +75,7 @@ const renderTimers = () => {
 const updateTimer = () => {
   timers[currentHost] += 1000;
 
-  const formattedTime = Math.round(timers[currentHost] / 1000);
-  timerElement.innerText = formattedTime;
+  renderTimers();
 };
 
 const startTimer = () => {
